f26a72c7fafc21befc0147a60cf04a4d7d244047
Jordan <mele@canva.com>
2023-12-05 10:36:17 +1100

    Optimise copying of `data` inputs in `npm_install` and `yarn_install` (#41)

---
+++ internal/npm_install/bulk_copy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu -o pipefail
+
+# The list of files to copy, relative to base so directories are preserved
+input_files_list="$1"
+# The base path which input files will be resolved from
+input_files_base="$2"
+# Destination directory for files
+out_dir="$3"
+
+tar --create --file - --directory "$input_files_base" --files-from "$input_files_list" | tar --extract --file - --directory "$out_dir"
